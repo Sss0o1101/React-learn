e78a3cbbfba49ed5dd227a2c928e9fe34322201d
alias <matusoyo2436@gmail.com>
2026-04-30 23:17:52 +0900

    import Display

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 //import Button from './Button.jsx'
 //import Button from './Button'
 import Button from './components/Button/Button.jsx'
+import Display from './components/Display/Display.jsx'
+
 
 
 function App() {
@@ -19,10 +21,15 @@ function App() {
     <>
       <h1>Hello World</h1>
       <Button type="button" disabled={false} onClick={handleClick} >
-        カウント: {count}
+        ボタン
       </Button>
+      <Display count={count} />
+
     </>
   )
 }
+
+
+
 
 export default App
